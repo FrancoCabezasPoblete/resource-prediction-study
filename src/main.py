d58980de939c0b05dd80ea2c25bc60fe22da730f
FrancoCabezasPoblete <francoale2010@gmail.com>
2024-04-21 20:37:39 -0400

    fix: get metrics

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+from icecream import ic
 from utils import (
     parse_arguments,
-    run_with_strace,
-    run_and_monitor_memory,
-    run_and_monitor_cpu
+    syscalls,
+    memory_usage,
+    cpu_usage,
+    execution_time
 )
+import csv
 import os
 
 args = parse_arguments()
@@ -11,18 +14,43 @@ args = parse_arguments()
 if not os.path.exists(args.directory):
     os.makedirs(args.directory)
 
-stdout, stderr = run_with_strace(args.command, os.path.join(args.directory, "strace.txt"))
-print(f"STDOUT: {stdout}")
-print(f"STDERR: {stderr}")
+def print_std(stdout, stderr):
+    if stdout:
+        ic(f"STDOUT: {stdout}")
+    if stderr:
+        ic(f"STDERR: {stderr}")
 
-memory_info, stdout, stderr = run_and_monitor_memory(args.command)
-print(f"Memory Info: {memory_info}")
-print(f"STDOUT: {stdout}")
-print(f"STDERR: {stderr}")
+ic(f"Running command: {args.command}")
 
-cpu_times, execution_time, stdout, stderr = run_and_monitor_cpu(args.command)
+stdout, stderr = syscalls(args.command, os.path.join(args.directory, "strace.txt"))
+print_std(stdout, stderr)
 
-print(f"CPU Times: {cpu_times}")
-print(f"Execution Time: {execution_time}")
-print(f"STDOUT: {stdout}")
-print(f"STDERR: {stderr}")
+memory_records, stdout, stderr = memory_usage(args.command)
+# Write the memory records to a file
+if memory_records:
+    with open(os.path.join(args.directory, "memory_usage.csv"), "w", newline='') as f:
+        fields = memory_records[0].keys()
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+        for record in memory_records:
+            writer.writerow(record)
+print_std(stdout, stderr)
+
+cpu_records, stdout, stderr = cpu_usage(args.command)
+# Write the CPU records to a file
+if cpu_records:
+    with open(os.path.join(args.directory, "cpu_usage.csv"), "w", newline='') as f:
+        fields = cpu_records[0].keys()
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+        for record in cpu_records:
+            writer.writerow(record)
+print_std(stdout, stderr)
+
+exec_time, stdout, stderr = execution_time(args.command, 10)
+# Write the execution times to a file
+with open(os.path.join(args.directory, "execution_time.csv"), "w") as f:
+    f.write("execution_time\n")
+    for record in exec_time:
+        f.write(f"{record}\n")
+print_std(stdout, stderr)
--- src/utils.py
+++ src/utils.py
@@ -9,27 +9,48 @@ def parse_arguments():
     parser.add_argument("command", nargs='+', help="Command to be executed.")
     return parser.parse_args()
 
-def run_with_strace(command, result_file):
+def syscalls(command, result_file):
     with subprocess.Popen(
         ["strace", "-o", result_file] + command, 
         stdout=subprocess.PIPE, 
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
     ) as proc:
         stdout, stderr = proc.communicate()
         return stdout, stderr
 
-def run_and_monitor_memory(command):
+def memory_usage(command, interval=1e-2):
+    memory_records = []
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     ps_process = psutil.Process(process.pid)
-    memory_info = ps_process.memory_info()
+    try:
+        while process.poll() is None:
+            memory_records.append(ps_process.memory_info()._asdict())
+            time.sleep(interval)  # Wait until the next record
+    except psutil.NoSuchProcess:
+        print("The process finished before the end of the monitoring.")
     stdout, stderr = process.communicate()
-    return memory_info, stdout, stderr
+    return memory_records, stdout, stderr
 
-def run_and_monitor_cpu(command):
-    start_time = time.time()
+def cpu_usage(command, interval=1e-2):
+    cpu_records = []
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     ps_process = psutil.Process(process.pid)
+    try:
+        while process.poll() is None:
+            cpu_records.append(ps_process.cpu_times()._asdict())
+            time.sleep(interval)
+    except psutil.NoSuchProcess:
+        print("The process finished before the end of the monitoring.")
     stdout, stderr = process.communicate()
-    end_time = time.time()
-    cpu_times = ps_process.cpu_times()
-    return cpu_times, end_time - start_time, stdout, stderr
+    return cpu_records, stdout, stderr
+
+
+def execution_time(command, iterations=1):
+    execution_times = []
+    for _ in range(iterations):
+        start_time = time.time()
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        end_time = time.time()
+        execution_times.append(end_time - start_time)
+    return execution_times, stdout, stderr
