6460ef86964c8336c3902d98b02c7e46f03cc69c
Frank <francoale2010@gmail.com>
2024-05-08 06:06:00 -0400

    fix: sql join windows 11

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#python /src/main.py matrix_multiplication python /benchmarks/matrix_multiplication/main.py 5000
+python /src/main.py matrix_multiplication python /benchmarks/matrix_multiplication/main.py 5000
 python /benchmarks/sql_join/init_db.py 1000 5000 /src/example.db
 python /src/main.py sql_join python /benchmarks/sql_join/main.py /src/example.db
-#python /src/main.py simulated_annealing /benchmarks/simulated_annealing/sa_bench $SA_INSTANCES/InstanceBEP-8-40-20-20.txt 100 1000 1 5
+python /src/main.py simulated_annealing /benchmarks/simulated_annealing/sa_bench $SA_INSTANCES/InstanceBEP-8-40-20-20.txt 100 1000 1 5
